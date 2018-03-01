4b5f96dbbe9eea8d8277b55bd191ab43a67326c4
CarterFitzsimons <cfitzsimons1@mail.csuchico.edu>
2018-02-28 19:14:14 -0800

    Program simulates a layer guesssing door C every time

--- main.cpp
+++ main.cpp
@@ -6,6 +6,23 @@ using namespace std;
 int main()
 {
   SecretDoor game;
+  int numPlays;
+  int wins = 0;
+  
+  cout<<"How many times do you want to play the game?"<<endl;
+  cin>>numPlays;
+
+  for(int i = 0;i < numPlays;i++){
+    game.newGame();
+    game.guessDoorC();
+    game.guessDoorC();
     
+    if(game.isWinner() == true){
+      wins++;
+    }
+  }
+  
+  cout<<"You won "<<wins<<" times."<<endl;
+
   return 0;
 }
